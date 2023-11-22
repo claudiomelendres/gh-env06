da2d8ad6e5496cd73d44a6ec56a24d3443cff9d4
claudiomelendres <claudiomelendres@gmail.com>
2023-11-22 19:06:35 -0400

    test1

--- data/database.js
+++ data/database.js
@@ -7,7 +7,9 @@ const dbPassword = process.env.MONGODB_PASSWORD || "jdaniel3210.";
 const dbName = process.env.MONGODB_DB_NAME || "eshop-database";
 
 // const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://cayo999:jdaniel3210.@cluster0.9xxlj.mongodb.net/eshop-database?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://cayo999:jdaniel3210.@cluster0.9xxlj.mongodb.net/eshop-database?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:jdaniel3210.@cluster0.9xxlj.mongodb.net/eshop-database?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 console.log("Trying to connect to db");
