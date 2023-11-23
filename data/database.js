112868987e1f963a9b8478fcf4c0f105c5caeabe
claudiomelendres <claudiomelendres@gmail.com>
2023-11-22 22:01:58 -0400

    added secrets

--- data/database.js
+++ data/database.js
@@ -1,14 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const clusterAddress =
-  process.env.MONGODB_CLUSTER_ADDRESS || "cluster0.9xxlj.mongodb.net";
-const dbUser = process.env.MONGODB_USERNAME || "cayo999";
-const dbPassword = process.env.MONGODB_PASSWORD || "jdaniel3210.";
-const dbName = process.env.MONGODB_DB_NAME || "eshop-database";
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://cayo999:jdaniel3210.@cluster0.9xxlj.mongodb.net/eshop-database?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://${dbUser}:jdaniel3210.@cluster0.9xxlj.mongodb.net/eshop-database?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
